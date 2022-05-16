e7aa3e8275f6e74f838ca660bc0af9e406693def
GelseyGhotra <100119133+GelseyGhotra@users.noreply.github.com>
2022-05-16 16:07:13 +0930

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,12 +16,9 @@ function setup() {
   world = engine.world;
 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-
-  //create a player object from the Player class.
   player = new Player(285, playerBase.body.position.y - 153, 50, 180);
 
- 
- 
+
   computerBase = new ComputerBase(
     width - 300,
     random(450, height - 300),
@@ -51,8 +48,8 @@ function draw() {
 
  
   playerBase.display();
-//call the display() function for the player object.
   player.display();
+  
 
   computerBase.display();
   computer.display();
